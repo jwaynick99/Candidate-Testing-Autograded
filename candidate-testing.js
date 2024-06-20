660c859f064c32ee2a77075fa308ba8975337ede
jwaynick99 <jwaynick99@gmail.com>
2024-06-20 16:53:59 -0500

    Format Change

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,7 +3,7 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName = "";
+let candidateName = " ";
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
@@ -57,7 +57,7 @@ function gradeQuiz(candidateAnswers) {
       numCorrectAnswers += 1
     }
   }
-  console.log(numCorrectAnswers)
+
   grade = numCorrectAnswers / questions.length * 100
   if (grade >= 80) {
     console.log(`Congrats! You've passed with a(n) ${grade}%`)
